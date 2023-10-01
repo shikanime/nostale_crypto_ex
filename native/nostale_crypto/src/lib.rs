ec859d97670e40ca8ef6ec208cf238e91a929cc1
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-10-01 22:28:44 +0000

    Add world_next/2
    Summary:
    
    Test Plan:

--- native/nostale_crypto/src/lib.rs
+++ native/nostale_crypto/src/lib.rs
@@ -1,28 +1,18 @@
 use rustler::{Binary, Env, NewBinary};
 
-const SEPARATOR: u8 = 0xd8;
+const LOGIN_SEPARATOR: u8 = 0xd8;
 
 #[rustler::nif]
 fn login_next<'a>(env: Env<'a>, raw: Binary<'a>) -> (Option<Binary<'a>>, Binary<'a>) {
-    match do_login_next(raw.as_slice()) {
+    match next(raw.as_slice(), LOGIN_SEPARATOR) {
         Some((packet, remaining)) => {
             let mut pb = NewBinary::new(env, packet.len());
             pb.as_mut_slice().copy_from_slice(packet);
             let mut rb = NewBinary::new(env, remaining.len());
             rb.as_mut_slice().copy_from_slice(remaining);
-
             (Some(pb.into()), rb.into())
         }
-        None => (None, raw)
-    }
-}
-
-fn do_login_next(raw: &[u8]) -> Option<(&[u8], &[u8])> {
-    let index = raw.iter().position(|&r| r == SEPARATOR);
-
-    match index {
-        Some(i) => Some((&raw[0..i], &raw[i + 1..])),
-        _ => None,
+        None => (None, raw),
     }
 }
 
@@ -52,7 +42,52 @@ fn do_login_decrypt(raw: &[u8]) -> Vec<u8> {
         .collect()
 }
 
+#[rustler::nif]
+fn world_next<'a>(env: Env<'a>, raw: Binary<'a>, key: u8) -> (Option<Binary<'a>>, Binary<'a>) {
+    let delimiter = pack_delimiter(cipher_mode(key), cipher_offset(key));
+    match next(raw.as_slice(), delimiter) {
+        Some((packet, remaining)) => {
+            let mut pb = NewBinary::new(env, packet.len());
+            pb.as_mut_slice().copy_from_slice(packet);
+            let mut rb = NewBinary::new(env, remaining.len());
+            rb.as_mut_slice().copy_from_slice(remaining);
+            (Some(pb.into()), rb.into())
+        }
+        None => (None, raw),
+    }
+}
+
+/// Decrypt the delimiter from a key.
+fn pack_delimiter(offset: u8, mode: u8) -> u8 {
+    match mode {
+        0 => 0xFF + offset,
+        1 => 0xFF - offset,
+        2 => 0xFF + offset ^ 0xC3,
+        3 => 0xFF - offset ^ 0xC3,
+        _ => unreachable!(),
+    }
+}
+
+/// Decrypt the offset from a key.
+fn cipher_offset(key: u8) -> u8 {
+    key & 0xFF
+}
+
+/// Decrypt the mode from a key.
+fn cipher_mode(key: u8) -> u8 {
+    (key >> 6) & 0x3
+}
+
+/// Get the next packet from a raw binary.
+fn next<'a>(raw: &[u8], delimiter: u8) -> Option<(&[u8], &[u8])> {
+    let index = raw.iter().position(|&r| r == delimiter);
+    match index {
+        Some(i) => Some((&raw[0..i], &raw[i + 1..])),
+        _ => None,
+    }
+}
+
 rustler::init!(
     "Elixir.NostaleCrypto.Native",
-    [login_next, login_encrypt, login_decrypt]
+    [login_next, login_encrypt, login_decrypt, world_next]
 );
